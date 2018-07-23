3743c1470a0c9a6693dddc9cacea10b3abfdf247
Marc-Daniel Ortega <globulon@gmail.com>
2018-07-23 22:23:42 +0100

    Dubbed saddle back search

--- src/main/scala/com/omd/fp/pearls/Saddleback.scala
+++ src/main/scala/com/omd/fp/pearls/Saddleback.scala
@@ -14,4 +14,25 @@ protected[pearls] trait Saddleback {
     case n if n > z ⇒ find1(acc)(u, v - 1)(f)(z)
     case _          ⇒ find1((u, v)::acc)(u + 1, v - 1)(f)(z)
   }
+
+
+  //Dubbed saddle back search
+  final def find2(f: (Int, Int) ⇒ Int)(z: Int): List[(Int, Int)] = find1(List.empty)(0, z)(f)(z)
+
+  @tailrec
+  private def find2(acc: List[(Int, Int)])(u: Int, v: Int)(f: (Int, Int) ⇒ Int)(z: Int): List[(Int, Int)] =
+    (bsearch(f(0,_))(z)(-1, v + 1), bsearch(f(_,0))(z)(u - 1, z + 1)) match {
+      case (_, n) if u > n       ⇒ acc
+      case _      if v < 0       ⇒ acc
+      case (m, _) if f(u,m) < z  ⇒ find2(acc)(u + 1, m)(f)(z)
+      case (m, _) if f(u, m) > z ⇒ find2(acc)(u, m - 1)(f)(z)
+      case (m, _)                ⇒ find2((u, v)::acc)(u + 1, m - 1)(f)(z)
+    }
+
+  @tailrec
+  private def bsearch(g: Int ⇒ Int)(z: Int)(min: Int, max: Int) : Int = (min, max) match {
+    case (a, b) if b == a + 1           ⇒ a
+    case (a, b) if g((a + b) / 2) <= z  ⇒ bsearch(g)(z)((a + b) / 2, b)
+    case (a, b)                         ⇒ bsearch(g)(z)(a, (a + b) / 2)
+  }
 }
--- src/test/scala/com/omd/fp/pearls/SaddlebackTest.scala
+++ src/test/scala/com/omd/fp/pearls/SaddlebackTest.scala
@@ -16,4 +16,19 @@ final class SaddlebackTest extends WordSpecLike with MustMatchers {
       find1(_ * _)(36).toSet must be (Set((6,6), (1,36), (3,12), (4,9), (12,3), (18,2), (9,4), (36,1), (2,18)))
     }
   }
+
+  "find 2" should {
+    "find all combinations for 10" in {
+      find2(_ * _)(10).toSet must be (Set((10,1), (5,2), (2,5), (1,10)))
+    }
+
+    "find all combinations for 17" in {
+      find2(_ * _)(17).toSet must be (Set((1,17), (17, 1)))
+    }
+
+    "find all combinations for  36" in {
+      find2(_ * _)(36).toSet must be (Set((6,6), (1,36), (3,12), (4,9), (12,3), (18,2), (9,4), (36,1), (2,18)))
+    }
+  }
+
 }
